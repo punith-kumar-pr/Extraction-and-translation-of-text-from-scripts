af985822cab9b4e3cf60fdb56bb68af1d998a5d0
Punith Kumar P R <punikumar2002@gmail.com>
2023-05-06 23:23:24 +0530

    Added kannada text extraction and conversion of it into other languages

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 // const ejs = require('ejs')
+const Tesseract = require('node-tesseract-ocr');
+
 
 const app = express();
 
@@ -10,8 +13,8 @@ const app = express();
 const homeRoute = require('./routes/home');
 const aboutRoute = require('./routes/about');
 const contactRoute = require('./routes/contact');
-const imgUploadRoute = require('./routes/uploadimg');
-const translateRoute = require('./routes/translate')
+// const imgUploadRoute = require('./routes/uploadimg');
+// const translateRoute = require('./routes/translate')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 
@@ -31,6 +34,33 @@ mongoose.connect('mongodb+srv://punith-kumar-pr:punith123@punith-cluster.c2shwte
 
 // Database connection ended
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// // Render a form that accepts an image file
+// app.get('/', (req, res) => {
+//   res.render('form');
+// });
+
+// Render a form that accepts an image file
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+ 
+// Handle form submission and extract text from image
+app.post('/ocr', (req, res) => {
+  const imageFile = req.files.image;
+  
+  Tesseract.recognize('a.jpg',{lang:'kan'})
+    .then(text => {
+      res.render('ocr', { text });
+    })
+    .catch(error => {
+      console.error(error);
+      res.render('error');
+    });
+});
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -40,8 +70,13 @@ app.use(express.json())
 app.use(homeRoute);
 app.use('/contact', contactRoute);
 app.use('/about', aboutRoute);
-app.use('/translate', translateRoute);
+// app.use('/translate', translateRoute);
 
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use((req, res, next) => {
     res.status(404).render(path.join(__dirname, 'views', '404.ejs'));
--- routes/translate.js
+++ routes/translate.js
@@ -2,22 +2,95 @@ const axios = require('axios');
 const express = require('express')
 const router = express.Router()
 
+// TESSERACT
+
+// ENDED TESSERACT
+
+const kan_txt=`ಕನ್ನಡ ಅಕ್ಷರಮಾಲೆಯು ಬ್ರಾಹ್ಮಿ ಲಿಪಿಯಿಂದ ಬೆಳೆದು ಬಂದಿದೆ. ಇದನ್ನು ಸ್ವರಗಳು, ಅನುಸ್ವಾರ, ವಿಸರ್ಗ, ವ್ಯಂಜನಗಳು, ಅವರ್ಗೀಯ ವ್ಯಂಜನಗಳೆಂದು ವಿಭಾಗಿಸಲಾಗಿದೆ. ಕನ್ನಡ ಅಕ್ಷರಮಾಲೆಯನ್ನು ಕನ್ನಡ ವರ್ಣಮಾಲೆಯೆಂದು ಕರೆಯಲಾಗುತ್ತದೆ. ನಾವು ಮಾತನಾಡುವ ಮಾತುಗಳೆಲ್ಲ ವಾಕ್ಯ
+ವಾಕ್ಯಗಳಾಗಿರುತ್ತವೆ.
+ವಾಕ್ಯಗಳು ಪದಗಳಿಂದ ಕೂಡಿರುತ್ತವೆ. ಪದಗಳು ಅಕ್ಷರಗಳಿಂದ ಕೂಡಿರುತ್ತವೆ. ಉದಾಹರಣೆಗೆ, ನಾನು ಶಾಲೆಗೆ ಹೋಗಿ
+ಬರುವೆನು. ಈ
+ವಾಕ್ಯದಲ್ಲಿ ನಾನು, ಶಾಲೆಗೆ, ಹೋಗಿ, ಬರುವೆನು, ಹೀಗೆ ನಾಲ್ಕು ಪದಗಳಿವೆ. ಒಂದೊಂದು ಪದದಲ್ಲೂ ಹಲವು
+ಅಕ್ಷರಗಳಿವೆ.`
 router.get('/',(req,res,next)=>{
-    let kan_txt=`ಕನ್ನಡ ಅಕ್ಷರಮಾಲೆಯು ಬ್ರಾಹ್ಮಿ ಲಿಪಿಯಿಂದ ಬೆಳೆದು ಬಂದಿದೆ. ಇದನ್ನು ಸ್ವರಗಳು, ಅನುಸ್ವಾರ, ವಿಸರ್ಗ, ವ್ಯಂಜನಗಳು, ಅವರ್ಗೀಯ ವ್ಯಂಜನಗಳೆಂದು ವಿಭಾಗಿಸಲಾಗಿದೆ. ಕನ್ನಡ ಅಕ್ಷರಮಾಲೆಯನ್ನು ಕನ್ನಡ ವರ್ಣಮಾಲೆಯೆಂದು ಕರೆಯಲಾಗುತ್ತದೆ. ನಾವು ಮಾತನಾಡುವ ಮಾತುಗಳೆಲ್ಲ ವಾಕ್ಯ
-    ವಾಕ್ಯಗಳಾಗಿರುತ್ತವೆ.
-    ವಾಕ್ಯಗಳು ಪದಗಳಿಂದ ಕೂಡಿರುತ್ತವೆ. ಪದಗಳು ಅಕ್ಷರಗಳಿಂದ ಕೂಡಿರುತ್ತವೆ. ಉದಾಹರಣೆಗೆ, ನಾನು ಶಾಲೆಗೆ ಹೋಗಿ
-    ಬರುವೆನು. ಈ
-    ವಾಕ್ಯದಲ್ಲಿ ನಾನು, ಶಾಲೆಗೆ, ಹೋಗಿ, ಬರುವೆನು, ಹೀಗೆ ನಾಲ್ಕು ಪದಗಳಿವೆ. ಒಂದೊಂದು ಪದದಲ್ಲೂ ಹಲವು
-    ಅಕ್ಷರಗಳಿವೆ.`
-const url = `http://127.0.0.1:5000/translate?text=${kan_txt}`; // Example Kannada text
+const url = `http://127.0.0.1:5000/translate?text=${kan_txt}&ln=en`; // Example Kannada text
 axios.get(url)
     .then(response => {
         console.log(response.data.translation); // Prints the translated text
+        // req.obt_img = req.query.id;
         res.render('extracted.ejs',{kan_txt:kan_txt,eng_txt : response.data.translation, img_url :req.query.id});
     })
     .catch(error => {
         console.error(error);
     });
 });
+
+
+// MULTIPLE LANGUAGES
+
+// HINDI
+router.get('/hi',(req,res,next)=>{
+    const url = `http://127.0.0.1:5000/translate?text=${kan_txt}&ln=HI`; // Example Kannada text
+    axios.get(url)
+        .then(response => {
+            console.log(response.data.translation); // Prints the translated text
+            res.render('extracted.ejs',{kan_txt:kan_txt,eng_txt : response.data.translation, img_url :req.query.id});
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    });
+
+// MALAYALAM
+router.get('/ml',(req,res,next)=>{
+    const url = `http://127.0.0.1:5000/translate?text=${kan_txt}&ln=ml`; // Example Kannada text
+    axios.get(url)
+        .then(response => {
+            console.log(response.data.translation); // Prints the translated text
+            res.render('extracted.ejs',{kan_txt:kan_txt,eng_txt : response.data.translation, img_url :req.query.id});
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    });
+
+// SPANISH
+router.get('/es',(req,res,next)=>{
+    const url = `http://127.0.0.1:5000/translate?text=${kan_txt}&ln=es`; // Example Kannada text
+    axios.get(url)
+        .then(response => {
+            console.log(response.data.translation); // Prints the translated text
+            res.render('extracted.ejs',{kan_txt:kan_txt,eng_txt : response.data.translation, img_url :req.query.id});
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    });
+
+// TAMIL
+router.get('/ta',(req,res,next)=>{
+    const url = `http://127.0.0.1:5000/translate?text=${kan_txt}&ln=ta`; // Example Kannada text
+    axios.get(url)
+        .then(response => {
+            console.log(response.data.translation); // Prints the translated text
+            res.render('extracted.ejs',{kan_txt:kan_txt,eng_txt : response.data.translation, img_url :req.query.id});
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    });
+
+// TELUGU
+router.get('/te',(req,res,next)=>{
+    const url = `http://127.0.0.1:5000/translate?text=${kan_txt}&ln=ta`; // Example Kannada text
+    axios.get(url)
+        .then(response => {
+            console.log(response.data.translation); // Prints the translated text
+            res.render('extracted.ejs',{kan_txt:kan_txt,eng_txt : response.data.translation, img_url :req.query.id});
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }); 
 
 module.exports = router;
